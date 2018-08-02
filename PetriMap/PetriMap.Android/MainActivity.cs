9cf83c6160a39c3000404b15d11f3bf0f56ccdf7
krz <marius.junak@scorpius-forge.de>
2018-08-02 15:42:39 +0200

    Add project files.

---
+++ PetriMap/PetriMap.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+
+namespace PetriMap.Droid
+{
+    [Activity(Label = "PetriMap", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry container)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
